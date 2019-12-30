b1a4cea8dcd2835249a654ee8b9e083e53a2ac4c
Nick Matvyeyev <mnasoft@gmail.com>
2019-12-30 11:45:02 +0200

    Пн дек 30 09:45:02 UTC 2019

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format
+ :scriba
+ :systems (:mnas-package)
+ :documents ((:title "mnas-package"
+		     :authors ("Nick Matvyeyev")
+		     :output-format (:type :multi-html :template :minima)
+		     :sources ("manual.scr"))))
