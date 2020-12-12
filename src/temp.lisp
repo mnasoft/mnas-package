8191c4dcf2638813b453798fbeab5b2fba21427f
Nick Matvyeyev <mnasoft@gmail.com>
2020-12-12 08:27:31 +0200

    Sat Dec 12 06:27:31 UTC 2020

--- src/temp.lisp
+++ src/temp.lisp
@@ -59,44 +59,79 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defparameter *a* (getf  *d* (first *d*) "SSSS"))
+(require :temperature-fild)
 
 (defparameter *d* (multiple-value-list
 		   (trivial-documentation:package-api :temperature-fild)))
 
 
+(defun td-doc (package)
+  (second (multiple-value-list
+	   (trivial-documentation:package-api package))))
+
 (defun td-foo (package)
-  (let ((p-list (second
-		  (multiple-value-list
-		   (trivial-documentation:package-api package))))
+  (let* ((p-list (td-doc package))
 	 (p-list-data (cdr p-list)))
     (loop :for i :in p-list      :by #'cddr
 	;; :collect i
 	;; :collect (format nil "~S" i)
 	:collect (mapcar #'string-downcase
 			 (mnas-string:split ":" (format nil "~S" i)))
-	;;(symbol-name i)
+	;; (symbol-name i)
 	  )))
+
+
+(nbutfirst '(1 2 3 4 5 6) 10)
+ 
+
+(defun nbutfirst (lst &optional (n 1))
+  	     (loop :for i :in lst
+		   :for j :from 0 :below n
+		   :collect i))
 
 (defun td-bar (package)
-  (let ((p-list-data (cdr (second
-			   (multiple-value-list
-			    (trivial-documentation:package-api package))))))
-    (loop :for j :in p-list-data :by #'cddr
-	  :collect (apply #'td-baz j)
-	  )))
-
-(defun td-baz (p-list)
-  p-list
-  )
+  (labels ((td-baz (p-list)
+	     (nbutfirst p-list)
+	     p-list))
+    (let ((p-list-data
+	    (cdr
+	     (second
+	      (multiple-value-list
+	       (trivial-documentation:package-api package))))))
+      (loop :for j :in p-list-data :by #'cddr
+	    :collect (apply #'td-baz j)))))
 
 (td-foo :temperature-fild/plot)
 
 (td-bar :temperature-fild/splot)
 
+(td-doc :temperature-fild/splot)
+
+splot-t-fild-relative-rect
+
+TEMPERATURE-FILD/SPLOT:SPLOT-T-FILD-RELATIVE-RECT
+ ((:KIND :GENERIC-FUNCTION :LAMBDA-LIST
+   (t03 t2 u-tm t-fild &key scale d-pts intervals hights ocr preamble)
+   :DOCUMENTATION NIL))
+
+splot-t-fild-relative-rect (t03 t2 u-tm (t-fild mtf/t-fild:<t-fild>)
+				       &key
+					 (scale mtf/core:*scale-smoothing*)
+					 (d-pts (mtf/core:d-pts-scale t-fild :scale scale))
+					 (intervals 100)
+					 (hights (list 1 0 intervals))
+					 (ocr (mtf/core:ocr-intervals t-fild :intervals intervals))
+					 (preamble nil))
+
 (mapcar #'string-downcase
 	(mnas-string:split ":" (format nil "~S" i)))
 
-(symbol-plist 
+(butlast '(1 2 3 4 5) 2)
 
+(documentation (find-package :temperature-fild/splot) t)
 
+(loop :for symbol :being the external-symbols
+	:in :temperature-fild/splot :collect symbol)
+
+(loop :for symbol :being the inter-sy
+	:in :temperature-fild/splot :collect symbol)
