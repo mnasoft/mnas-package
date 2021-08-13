ca37a0547fc26a1eff3548792271bb0864d37280
Nick Matvyeyev <mnasoft@gmail.com>
2021-08-13 16:42:31 +0300

    13 авг 2021 г. 13:42:31

--- docs/manifest.lisp
+++ docs/manifest.lisp
@@ -3,7 +3,7 @@
    :systems (:MNAS-PACKAGE)
    :documents ((:title "Mnas-Package"
 	        :authors ("Nick Matvyeyev")
-	        :output-format (:TYPE :MULTI-HTML :TEMPLATE :MINIMA) 
+	        :output-format (:TYPE :MULTI-HTML :TEMPLATE :GAMMA) 
                 :sources ("mnas-package.scr" "mnas-package-view.scr"
                           "mnas-package-sys.scr" "mnas-package-sec.scr"
                           "mnas-package-pkg.scr" "mnas-package-obj.scr"
@@ -11,7 +11,7 @@
                           "mnas-package-graph.scr" "mnas-package-sys-graph.scr"
                           "mnas-package-view-graph.scr"
                           "mnas-package-make-graph.scr"
-                          "mnas-package-pkg-graph.scr"
                           "mnas-package-obj-graph.scr"
+                          "mnas-package-pkg-graph.scr"
                           "mnas-package-sec-graph.scr")
                 )))
