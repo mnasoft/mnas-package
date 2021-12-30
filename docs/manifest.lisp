47293e4fc5c54b531c610d965989ed93b7b7e3c3
Nick Matvyeyev <mnasoft@gmail.com>
2021-12-30 13:51:12 +0200

    30 дек 2021 г. 11:51:12

---
+++ docs/manifest.lisp
@@ -0,0 +1,8 @@
+(:docstring-markup-format
+   :scriba
+   :systems (:MNAS-PACKAGE)
+   :documents ((:title "Mnas-Ffile-Dialog"
+	        :authors ("Nick Matvyeyev")
+	        :output-format (:TYPE :MULTI-HTML :TEMPLATE :GAMMA) 
+                :sources ("mnas-file-dialog.scr" "mnas-file-dialog-graph.scr")
+                )))
