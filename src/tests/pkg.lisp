1af5016c850874033bb9b2854503d55e99cfd8d7
Mykola Matvyeyev <mnasoft@gmail.com>
2022-07-10 17:11:35 +0300

    Sun Jul 10 17:11:35 EEST 2022 added some tests

--- src/tests/pkg.lisp
+++ src/tests/pkg.lisp
@@ -8,99 +8,159 @@
 
 (def-test package-by-type-length ()
   (let ((pkg (find-package :mnas-package/example)))
-    (is-true (eq  6 (length (mnas-package/pkg:package-variables :mnas-package/example :internal t))))
-    (is-true (eq  3 (length (mnas-package/pkg:package-variables :mnas-package/example ))))
+    (is-true (eq  6 (length (mnas-package/pkg:package-variables pkg :internal t))))
+    (is-true (eq  3 (length (mnas-package/pkg:package-variables pkg ))))
     
-    (is-true (eq  6 (length (mnas-package/pkg:package-functions :mnas-package/example :internal t))))
-    (is-true (eq  3 (length (mnas-package/pkg:package-functions :mnas-package/example))))
+    (is-true (eq  6 (length (mnas-package/pkg:package-functions pkg :internal t))))
+    (is-true (eq  3 (length (mnas-package/pkg:package-functions pkg))))
     
-    (is-true (eq  6 (length (mnas-package/pkg:package-macroses :mnas-package/example :internal t))))
-    (is-true (eq  3 (length (mnas-package/pkg:package-macroses :mnas-package/example))))
+    (is-true (eq  6 (length (mnas-package/pkg:package-macroses pkg :internal t))))
+    (is-true (eq  3 (length (mnas-package/pkg:package-macroses pkg))))
     
-    (is-true (eq  3 (length (mnas-package/pkg:package-setf-functions :mnas-package/example :internal t))))
-    (is-true (eq  3 (length (mnas-package/pkg:package-setf-functions :mnas-package/example))))
+    (is-true (eq  3 (length (mnas-package/pkg:package-setf-functions pkg :internal t))))
+    (is-true (eq  3 (length (mnas-package/pkg:package-setf-functions pkg))))
     
-    (is-true (eq  11 (length (mnas-package/pkg:package-generics :mnas-package/example :internal t))))
-    (is-true (eq  3 (length (mnas-package/pkg:package-generics :mnas-package/example))))
+    (is-true (eq  11 (length (mnas-package/pkg:package-generics pkg :internal t))))
+    (is-true (eq  3 (length (mnas-package/pkg:package-generics pkg))))
+    
+    (is-true (eq  6 (length (mnas-package/pkg:package-setf-generics pkg :internal t))))
+    (is-true (eq  3 (length (mnas-package/pkg:package-setf-generics pkg))))
+    
+    (is-true (eq  4 (length (mnas-package/pkg:package-methods pkg :internal t))))
+    (is-true (eq  4 (length (mnas-package/pkg:package-methods pkg))))
 
-    (is-true (eq  6 (length (mnas-package/pkg:package-setf-generics :mnas-package/example :internal t))))
-    (is-true (eq  3 (length (mnas-package/pkg:package-setf-generics :mnas-package/example))))
-    
-    (is-true (eq  4 (length (mnas-package/pkg:package-methods :mnas-package/example :internal t))))
-    (is-true (eq  4 (length (mnas-package/pkg:package-methods :mnas-package/example))))
-
-    (is-true (eq  6 (length (mnas-package/pkg:package-classes :mnas-package/example :internal t))))
-    (is-true (eq  3 (length (mnas-package/pkg:package-classes :mnas-package/example))))
+    (is-true (eq  6 (length (mnas-package/pkg:package-classes pkg :internal t))))
+    (is-true (eq  3 (length (mnas-package/pkg:package-classes pkg))))
     ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(def-test package-variables ()
+  "(mnas-package/pkg:package-variables :mnas-package/example :internal t) "
+  (let ((pkg (find-package :mnas-package/example))
+        (vars '(MNAS-PACKAGE/EXAMPLE:*V-A-EXP*
+                MNAS-PACKAGE/EXAMPLE:*V-C-EXP*
+                MNAS-PACKAGE/EXAMPLE:*V-B-EXP*
+                MNAS-PACKAGE/EXAMPLE::*V-A-INT*
+                MNAS-PACKAGE/EXAMPLE::*V-C-INT*
+                MNAS-PACKAGE/EXAMPLE::*V-B-INT*)))
+        (loop :for i :in (mnas-package/pkg:package-variables pkg :internal t)
+          :do
+             (is-true (find i vars :test #'equal)))))
 
-(mnas-package/pkg:package-variables :mnas-package/example :internal t)
-#|
-'(MNAS-PACKAGE/EXAMPLE:*A*
-  MNAS-PACKAGE/EXAMPLE::*C*
-  MNAS-PACKAGE/EXAMPLE::*B*)
-|#
-(mnas-package/pkg:package-functions :mnas-package/example :internal t)
-#|
-'(#<FUNCTION MNAS-PACKAGE/EXAMPLE:BAZ>
-  #<FUNCTION MNAS-PACKAGE/EXAMPLE:BAZ-SHORT>
-  #<FUNCTION MNAS-PACKAGE/EXAMPLE::BAR>
-  #<FUNCTION MNAS-PACKAGE/EXAMPLE::BAR-SHORT>
-  #<FUNCTION MNAS-PACKAGE/EXAMPLE::FOO-SHORT>
-  #<FUNCTION MNAS-PACKAGE/EXAMPLE::FOO>)
-|#
-(mnas-package/pkg:package-macroses :mnas-package/example :internal t)
-#|
-'(#<FUNCTION (MACRO-FUNCTION MNAS-PACKAGE/EXAMPLE:MAK-A) {52EAB01B}>
-  #<FUNCTION (MACRO-FUNCTION MNAS-PACKAGE/EXAMPLE::MAK-A-SHORT) {52EB09AB}>)
-|#
+(def-test package-functions ()
+  "(mnas-package/pkg:package-functions :mnas-package/example :internal t)
+ "
+  (let ((pkg (find-package :mnas-package/example))
+        (funcs (list #'MNAS-PACKAGE/EXAMPLE:F-A-EXP
+                     #'MNAS-PACKAGE/EXAMPLE:F-B-EXP
+                     #'MNAS-PACKAGE/EXAMPLE:F-C-EXP
+                     #'MNAS-PACKAGE/EXAMPLE::F-C-INT
+                     #'MNAS-PACKAGE/EXAMPLE::F-B-INT
+                     #'MNAS-PACKAGE/EXAMPLE::F-A-INT)))
+    (loop :for i :in  (mnas-package/pkg:package-functions pkg :internal t)
+          :do
+             (is-true (find i funcs :test #'equal)))))
 
-(mnas-package/pkg:package-setf-functions :mnas-package/example :internal t)
-#|
-'(#<FUNCTION (SETF MNAS-PACKAGE/EXAMPLE::SETF-FOO)>
-  #<FUNCTION (SETF MNAS-PACKAGE/EXAMPLE::FOO)>)
-|#
+(def-test package-macroses ()
+  "(mnas-package/pkg:package-macroses :mnas-package/example :internal t)
+ "
+  (let ((pkg (find-package :mnas-package/example))
+        (maks (list (MACRO-FUNCTION 'MNAS-PACKAGE/EXAMPLE:K-B-EXP)
+                    (MACRO-FUNCTION 'MNAS-PACKAGE/EXAMPLE:K-C-EXP)
+                    (MACRO-FUNCTION 'MNAS-PACKAGE/EXAMPLE:K-A-EXP)
+                    (MACRO-FUNCTION 'MNAS-PACKAGE/EXAMPLE::K-A-INT)
+                    (MACRO-FUNCTION 'MNAS-PACKAGE/EXAMPLE::K-B-INT)
+                    (MACRO-FUNCTION 'MNAS-PACKAGE/EXAMPLE::K-C-INT))))
+    (loop :for i :in  (mnas-package/pkg:package-macroses pkg :internal t)
+          :do
+             (is-true (find i maks :test #'equal)))))
 
-(mnas-package/pkg:package-setf-generics :mnas-package/example :internal t)
-#|
-'(#<STANDARD-GENERIC-FUNCTION (COMMON-LISP:SETF MNAS-PACKAGE/EXAMPLE:M-FOO) (0)>
-  #<STANDARD-GENERIC-FUNCTION (COMMON-LISP:SETF MNAS-PACKAGE/EXAMPLE::SM-FOO) (0)>)
-|#
+(def-test package-setf-functions ()
+  "(mnas-package/pkg:package-setf-functions :mnas-package/example :internal t)
+ "
+  (let ((pkg (find-package :mnas-package/example))
+        (f-setf (list
+                 #'(SETF MNAS-PACKAGE/EXAMPLE:F-B-EXP)
+                 #'(SETF MNAS-PACKAGE/EXAMPLE:F-A-EXP)
+                 #'(SETF MNAS-PACKAGE/EXAMPLE:F-C-EXP))))
+        (loop :for i :in  (mnas-package/pkg:package-setf-functions pkg :internal t)
+          :do
+             (is-true (find i f-setf :test #'equal)))))
 
-(mnas-package/pkg:package-generics :mnas-package/example :internal t)
-#|
-'(#<STANDARD-GENERIC-FUNCTION MNAS-PACKAGE/EXAMPLE:M-FOO (8)>
-  #<STANDARD-GENERIC-FUNCTION MNAS-PACKAGE/EXAMPLE::M-FOO-SHORT (8)>)
-|#
+(def-test package-generics ()
+  "(mnas-package/pkg:package-generics :mnas-package/example :internal t)
+ "
+  (let ((pkg (find-package :mnas-package/example))
+        (gens (list #'MNAS-PACKAGE/EXAMPLE:M-C-EXP
+                      #'MNAS-PACKAGE/EXAMPLE:M-B-EXP
+                      #'MNAS-PACKAGE/EXAMPLE:M-A-EXP
+                      #'MNAS-PACKAGE/EXAMPLE::M-FOO
+                      #'MNAS-PACKAGE/EXAMPLE::<C-C-INT>-C
+                      #'MNAS-PACKAGE/EXAMPLE::M-B-INT
+                      #'MNAS-PACKAGE/EXAMPLE::M-A-INT
+                      #'MNAS-PACKAGE/EXAMPLE::M-FOO-SHORT
+                      #'MNAS-PACKAGE/EXAMPLE::<C-B-INT>-B
+                      #'MNAS-PACKAGE/EXAMPLE::M-C-INT
+                      #'MNAS-PACKAGE/EXAMPLE::<C-A-INT>-A)))
+        (loop :for i :in (mnas-package/pkg:package-generics pkg :internal t)
+          :do
+             (is-true (find i gens :test #'equal)))))
 
-(mnas-package/pkg:package-methods :mnas-package/example :internal t)
-#|
-'(#<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE:M-FOO :AROUND (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> T) {1006B58A13}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE:M-FOO :BEFORE (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> T) {1006B58A23}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE:M-FOO :AFTER (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> T) {1006B58A33}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE:M-FOO (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> T) {1006B58A43}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE:M-FOO :AROUND (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> MNAS-PACKAGE/EXAMPLE:<C>) {1006B59003}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE:M-FOO :BEFORE (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> MNAS-PACKAGE/EXAMPLE:<C>) {1006B59013}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE:M-FOO :AFTER (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> MNAS-PACKAGE/EXAMPLE:<C>) {1006B59023}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE:M-FOO (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> MNAS-PACKAGE/EXAMPLE:<C>) {1006B59033}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE::M-FOO-SHORT :AROUND (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> T) {1006B589D3}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE::M-FOO-SHORT :BEFORE (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> T) {1006B589E3}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE::M-FOO-SHORT :AFTER (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> T) {1006B589F3}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE::M-FOO-SHORT (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> T) {1006B58A03}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE::M-FOO-SHORT :AROUND (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> MNAS-PACKAGE/EXAMPLE:<C>) {1006B58FC3}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE::M-FOO-SHORT :BEFORE (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> MNAS-PACKAGE/EXAMPLE:<C>) {1006B58FD3}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE::M-FOO-SHORT :AFTER (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> MNAS-PACKAGE/EXAMPLE:<C>) {1006B58FE3}>
-  #<STANDARD-METHOD MNAS-PACKAGE/EXAMPLE::M-FOO-SHORT (MNAS-PACKAGE/EXAMPLE::<A> MNAS-PACKAGE/EXAMPLE::<B> MNAS-PACKAGE/EXAMPLE:<C>) {1006B58FF3}>)
-|#
+(def-test package-setf-generics ()
+  "(mnas-package/pkg:package-setf-generics :mnas-package/example :internal t)
+ "
+  (let ((pkg (find-package :mnas-package/example))
+        (g-setf (list #'(SETF MNAS-PACKAGE/EXAMPLE:M-C-EXP)
+                    #'(SETF MNAS-PACKAGE/EXAMPLE:M-B-EXP)
+                    #'(SETF MNAS-PACKAGE/EXAMPLE:M-A-EXP)
+                    #'(SETF MNAS-PACKAGE/EXAMPLE::<C-C-INT>-C)
+                    #'(SETF MNAS-PACKAGE/EXAMPLE::<C-B-INT>-B)
+                    #'(SETF MNAS-PACKAGE/EXAMPLE::<C-A-INT>-A))))
+       
+        (loop :for i :in (mnas-package/pkg:package-setf-generics pkg :internal t)
+          :do
+             (is-true (find i g-setf :test #'equal)))))
 
-(mnas-package/pkg:package-classes :mnas-package/example :internal t)
-#|
-'(#<STANDARD-CLASS MNAS-PACKAGE/EXAMPLE:<C>> ;
-  #<STANDARD-CLASS MNAS-PACKAGE/EXAMPLE::<A>> ;
-  #<STANDARD-CLASS MNAS-PACKAGE/EXAMPLE::<B>> ;
-  #<STANDARD-CLASS MNAS-PACKAGE/EXAMPLE::<B-SHORT>> ;
-  #<STANDARD-CLASS MNAS-PACKAGE/EXAMPLE::<C-SHORT>> ;
-  #<STANDARD-CLASS MNAS-PACKAGE/EXAMPLE::<A-SHORT>>) ;
-|#
+
+(def-test package-methods ()
+  "(mnas-package/pkg:package-methods :mnas-package/example :internal t)
+ "
+  (let ((pkg (find-package :mnas-package/example))
+        (mets (list
+               (find-method #'MNAS-PACKAGE/EXAMPLE:M-A-EXP '()
+                            (mapcar #'find-class '(MNAS-PACKAGE/EXAMPLE::<C-A-INT>
+                                                   MNAS-PACKAGE/EXAMPLE::<C-B-INT>
+                                                   MNAS-PACKAGE/EXAMPLE:<C-C-EXP>)))
+               (find-method #'MNAS-PACKAGE/EXAMPLE:M-B-EXP '()
+                            (mapcar #'find-class '(MNAS-PACKAGE/EXAMPLE::<C-A-INT>
+                                                   MNAS-PACKAGE/EXAMPLE::<C-B-INT>
+                                                   MNAS-PACKAGE/EXAMPLE:<C-C-EXP>)))
+               (find-method #'MNAS-PACKAGE/EXAMPLE:M-C-EXP '()
+                            (mapcar #'find-class '(MNAS-PACKAGE/EXAMPLE::<C-A-INT>
+                                                   MNAS-PACKAGE/EXAMPLE::<C-B-INT>
+                                                   MNAS-PACKAGE/EXAMPLE::<C-C-INT>)))
+               (find-method #'MNAS-PACKAGE/EXAMPLE:M-C-EXP '()
+                            (mapcar #'find-class '(MNAS-PACKAGE/EXAMPLE:<C-A-EXP>
+                                                   MNAS-PACKAGE/EXAMPLE:<C-B-EXP>
+                                                   MNAS-PACKAGE/EXAMPLE:<C-C-EXP>))))))
+       
+        (loop :for i :in (mnas-package/pkg:package-methods pkg :internal t)
+          :do
+             (is-true (find i mets :test #'equal)))))
+
+(def-test package-classes ()
+  "(mnas-package/pkg:package-classes :mnas-package/example :internal t)
+ "
+  (let ((pkg (find-package :mnas-package/example))
+        (mets (list
+               (find-class 'MNAS-PACKAGE/EXAMPLE:<C-A-EXP>)
+               (find-class 'MNAS-PACKAGE/EXAMPLE:<C-C-EXP>)
+               (find-class 'MNAS-PACKAGE/EXAMPLE:<C-B-EXP>)
+               (find-class 'MNAS-PACKAGE/EXAMPLE::<C-C-INT>)
+               (find-class 'MNAS-PACKAGE/EXAMPLE::<C-A-INT>)
+               (find-class 'MNAS-PACKAGE/EXAMPLE::<C-B-INT>))))
+       
+    (loop :for i :in (mnas-package/pkg:package-classes pkg :internal t)
+          :do
+             (is-true (find i mets :test #'equal)))))
