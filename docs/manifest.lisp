ee9a237243202f475569883cc41fe459389bbb22
Mykola Matvyeyev <mnasoft@gmail.com>
2024-11-06 07:02:59 +0200

    Ср 06 ноя 2024 05:02:59 UTC

--- docs/manifest.lisp
+++ docs/manifest.lisp
@@ -8,10 +8,11 @@
                           "mnas-package-sys.scr" "mnas-package-sec.scr"
                           "mnas-package-pkg.scr" "mnas-package-obj.scr"
                           "mnas-package-make.scr" "mnas-package-example.scr"
-                          "mnas-package-graph.scr" "mnas-package-sys-graph.scr"
-                          "mnas-package-view-graph.scr"
+                          "mnas-package-graph.scr"
                           "mnas-package-make-graph.scr"
-                          "mnas-package-pkg-graph.scr"
                           "mnas-package-obj-graph.scr"
-                          "mnas-package-sec-graph.scr")
+                          "mnas-package-pkg-graph.scr"
+                          "mnas-package-sec-graph.scr"
+                          "mnas-package-sys-graph.scr"
+                          "mnas-package-view-graph.scr")
                 )))
